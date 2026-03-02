19a5a650c7b2e2cffee1e7a050b85f2a10e84615
ranvijaytiwari11 <208862231+ranvijaytiwari11@users.noreply.github.com>
2026-03-02 21:52:10 +0530

    day 5cp31

---
+++ 800-1200/11000 rated.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+void solve() {
+    ll n, k;
+    cin >> n >> k;
+
+    vector<ll> v(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    map<ll, bool> mp;
+
+    for (auto it : v) {
+        mp[it] = true;
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (mp.find(v[i] - k) != mp.end()) {
+            cout << "YES" << endl;
+            return;
+        }
+    }
+
+    cout << "NO" << endl;
+}
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
