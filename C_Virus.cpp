fe5650116d8d3f8337b50abd75dddfc983c955a0
ranvijaytiwari11 <208862231+ranvijaytiwari11@users.noreply.github.com>
2026-04-08 19:14:31 +0530

    Add files via upload

---
+++ C_Virus.cpp
@@ -0,0 +1,23 @@
+// ================================================
+// ॐ Namah Parvati Pataye Har Har Mahadev
+// Inspiration : न द्वेष्टि न काङ्क्षति - स शिवः।
+// ================================================
+// Author      : Ranvijay Tiwari
+// Language    : C++
+// Focus       : DSA & Competitive Programming
+// Status      : Always Learning | Always Improving
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,m;
+        cin>>n>>m;
+        vector<int>a(m);
+        for(int i=0;i<m;i++)cin>>a[i];
+        
+    }
+    return 0;
+}
